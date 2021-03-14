5e770df90f9be81f24e8efd9f11a07a7eec4e530
imhilla <hillaryodhiambo@gmail.com>
2021-03-14 21:44:13 +0300

    add right option container

--- src/Components/RightTop.js
+++ src/Components/RightTop.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../Styles/RightTop.css';
+import RightOptions from './RightOptions';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 
 function RightTop() {
   return (
     <div className="rightTopContainer">
-      most right
+      <div className="rightTopDiv">
+        <RightOptions Icon={HelpOutlineIcon} title="HELP CENTER" description="Guide To Customer Care" />
+        <RightOptions Icon={DonutLargeIcon} title="EASY RETURN" description="Quick Refund" />
+        <RightOptions Icon={MonetizationOnIcon} title="SELL ON JUMIA" description="Millions Of Visitors" />
+      </div>
+      <div className="rightBottomDiv">
+      </div>
     </div>
   )
 }
