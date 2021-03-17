90b9d8acdcfd3a9449cef9685ede0c5931a12d8a
imhilla <hillaryodhiambo@gmail.com>
2021-03-17 13:18:59 +0300

    add billboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Billboard from './Components/Billboard';
 import Header from './Components/Header';
 import TopSection from './Components/TopSection';
 
 function App() {
   return (
     <div className="App">
+       <Billboard />
        <Header />
        <TopSection />
     </div>
